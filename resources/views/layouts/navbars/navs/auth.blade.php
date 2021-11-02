57578252481d1c14170780da604e56705e5ef9a2
sraffz <90593351+sraffz@users.noreply.github.com>
2021-11-02 23:40:25 +0800

    2112021 night

--- resources/views/layouts/navbars/navs/auth.blade.php
+++ resources/views/layouts/navbars/navs/auth.blade.php
@@ -53,7 +53,7 @@
             </p>
           </a>
           <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownProfile">
-            <a class="dropdown-item" href="{{ url('tetapan') }}">{{ __('Profile') }}</a>
+            <a class="dropdown-item" href="{{ url('tetapan') }}">{{ __('Profil') }}</a>
             {{-- <a class="dropdown-item" href="#">{{ __('Settings') }}</a> --}}
             <div class="dropdown-divider"></div>
             <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">{{ __('Log out') }}</a>
